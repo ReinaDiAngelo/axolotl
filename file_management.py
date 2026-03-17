34a09c5f3c202b2348ece040fe09f8f4a15c4ff0
Rei <264700405+ReinaDiAngelo@users.noreply.github.com>
2026-03-17 18:48:45 -0500

    feat: add file management functions

---
+++ file_management.py
@@ -0,0 +1,48 @@
+import os
+
+class FileManagement():
+
+    # Class Variables
+    file_names = []     # list with all file/subdirectory paths
+    current_index = 0   # current image that is displayed
+    list_length = 0
+
+    def __init__(self):
+        super().__init__()
+        self.file_names = self.get_files("anime_stuff")
+        self.list_length = len(self.file_names)
+
+    # get_files() --> Given a directory name, create a list of file names (.png, .jpg) within file_names
+    def get_files(self, directory_path: str):
+        file_names = []     # reset the list
+
+        # loop through a given directory + append file/subdirectory names to file_names
+        for entry in os.listdir(directory_path):
+            full_path = os.path.join(directory_path, entry)
+
+            # Base Case --> If file found, add to file_names
+            if os.path.isfile(full_path):
+                file_names.append(full_path)
+            # Recursive Case --> If directory found, check files within, then add to file_names
+            elif os.path.isdir(full_path):
+                file_names.extend(self.get_files(full_path))
+
+        return file_names
+
+    #
+    def get_current_image(self):
+        return self.file_names[self.current_index]
+
+    # 
+    def set_previous_image(self):
+        if self.current_index == 0:
+            pass
+        else:
+            self.current_index -= 1
+
+    # 
+    def set_next_image(self):
+        if self.current_index == self.list_length - 1:
+            pass
+        else:
+            self.current_index += 1
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from file_management import FileManagement
 
 # MyLayout --> 
 class MyLayout(FloatLayout):
-    pass
+    # Object Instantiations
+    previous_image = ObjectProperty(None)
+    next_image = ObjectProperty(None)
+    display_image = ObjectProperty(None)
+    file_names = FileManagement()
 
+
+    def __init__(self):
+        super().__init__()
+        self.display_image.source = self.file_names.get_current_image()
+
+    def prev_img(self):
+        self.file_names.set_previous_image()
+        self.display_image.source = self.file_names.get_current_image()
+        print(self.previous_image.text)
+
+    def next_img(self):
+        self.file_names.set_next_image()
+        self.display_image.source = self.file_names.get_current_image()
+        print(self.next_image.text)
 
 # AxolApp --> 
 class AxolApp(App):
